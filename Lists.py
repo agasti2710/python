33654c5f970c9650fd7f363189587290a895255a
agasti2710 <65022349+agasti2710@users.noreply.github.com>
2020-05-09 23:11:19 +0530

    Created lists.py
    Assigned elements to different lists

---
+++ Lists.py
@@ -0,0 +1,14 @@
+list1=[12,3,4,7]
+print(list1)
+list1.append(11)
+print("new list1 is:",list1)
+
+list2=["apple","orange"]
+print(list2)
+list2.insert(1,"peach")
+print("new list2 is:",list2)
+
+list3=[]
+for i in range(0,11,2):
+	list3.append(i)	
+print("list3 is:",list3)	
